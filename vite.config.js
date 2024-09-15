8e934ecf9332d35bead73ce6ff82f551da071177
Vanessahyc <hyc011224@gmail.com>
2024-09-15 14:35:29 -0700

    update: vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/blackpink_album_collections/'
+  base: '/COMP3170_Lab1/'
 })
